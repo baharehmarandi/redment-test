2e54d97bfca535e011877d37e5392559c1bcabda
baharehmarandi <70742323+baharehmarandi@users.noreply.github.com>
2023-09-04 23:08:14 +0330

    finish desktop header.

--- src/app/landing/landing.module.ts
+++ src/app/landing/landing.module.ts
@@ -2,13 +2,17 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { LandingPageComponent } from './pages/landing-page/landing-page.component';
 import { HeaderComponent } from './components/header/header.component';
+import { DescriptionComponent } from './components/description/description.component';
+import { MobileHeaderComponent } from './components/mobile-header/mobile-header.component';
 
 
 
 @NgModule({
   declarations: [
     LandingPageComponent,
-    HeaderComponent
+    HeaderComponent,
+    DescriptionComponent,
+    MobileHeaderComponent
   ],
   exports: [
     LandingPageComponent
